1f564b8413e8fb1b32ccea05397bba96a06a1e32
anubharoy55 <46259057+anubharoy55@users.noreply.github.com>
2022-06-13 12:39:58 +0530

    Update ChromeReviewsSentimentAnalysisCopy.py

--- ChromeReviewsSentimentAnalysisCopy.py
+++ ChromeReviewsSentimentAnalysisCopy.py
@@ -7,7 +7,7 @@ Created on Fri Jun 10 11:49:57 2022
 
 import pandas as pd
 import nltk
-nltk.download('stopwords')
+#nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import re
